5fa7c1cb897c570a124130f6df1ad44a3cb957a8
Maxi <requenamaximiliano@gmail.com>
2021-09-25 15:59:31 -0300

    add

---
+++ api/src/controllers/activity.js
@@ -0,0 +1,34 @@
+const { Activity, Country} = require('../db');
+const { v4: uuidv4 } = require('uuid');
+
+async function activityCreate(req, res, next){
+
+    const { name, difficulty, duration, season, countryId} = req.body; 
+
+    try {
+
+        let id = uuidv4;
+        let newActivity = await Activity.create({
+            name,
+            difficulty, 
+            duration,
+            season,
+            id
+        })
+        const country = Country.findOne({
+            where:{
+                id :countryId
+            }
+        })
+        newActivity.addCountry(country)
+        res.json(newActivity);
+        
+    } catch (error) {
+        next(error);
+        console.log("newActivityError")
+    }
+}
+
+module.exports = {
+    activityCreate
+}
---
+++ api/src/controllers/country.js
@@ -0,0 +1,4 @@
+const {Country, Activity} = require('../db');
+const axios = require('axios');
+const { Op } = require('sequalize');
+
