fdc8fa1bf7c4fb8e1ec0884600658b687d0a01c5
Maxi <requenamaximiliano@gmail.com>
2021-09-25 14:44:31 -0300

    models

---
+++ api/src/models/Activity.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('activity', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+        difficulty: {
+            type: DataTypes.ENUM('1', '2', '3', '4', '5'),
+        },
+        duration: {
+            type: DataTypes.STRING,
+        },
+        season: {
+            type: DataTypes.ENUM('Summer', 'Fall', 'Winter', 'Spring')
+        }
+    })
+}
